5cbb483addc962e2a79068876a58415e7309cde4
unknown <akash.felix@gmail.com>
2021-08-16 11:24:59 +0530

    finished auth flow

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
 const express = require('express');
+const authRoutes = require('./routes/authRoutes');
+require('./models/User');
+require('./services/passport');
+const keys = require('./config/keys');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 const app = express();
+const mongoose = require('mongoose');
 
-app.get('/', (req, res) => {
-  res.send({ hi: 'there' });
+mongoose.connect(keys.mongoURI, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
 });
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey],
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+authRoutes(app);
+mongoose.connect(keys.mongoURI);
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
